70eb45fc9f42a101c96f4baa13a4943d6796de08
itc-s24001 <s24001@std.it-college.ac.jp>
2025-05-16 12:30:47 +0900

    課題

---
+++ D48Q22.java
@@ -0,0 +1,9 @@
+enum Connection {OFFLINE, ONLINE}
+
+public class D48Q22 {
+    public static void main(String[] args) {
+        Connection s = Connection.OFFLINE;
+        s = Connection.ONLINE;
+        System.out.println(s);
+    }
+}
