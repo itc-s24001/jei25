d820dbb7da7022c78762a53607c302b4d494ac06
itc-s24001 <s24001@std.it-college.ac.jp>
2025-05-30 11:56:26 +0900

    課題

---
+++ D5023kadai.java
@@ -0,0 +1,13 @@
+public class D5023kadai {
+    String code;
+    public static void main(String[] args) {
+        D5023kadai obj = new D5023kadai();
+        obj.setCode("code");
+
+        System.out.println(obj.code);
+    }
+
+    void setCode(String c) {
+        code = c;
+    }
+}
