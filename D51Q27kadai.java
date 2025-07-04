167467908f82ab8f2696871e2648ee45d9ea8fe8
itc-s24001 <s24001@std.it-college.ac.jp>
2025-07-04 10:56:11 +0900

    課題

---
+++ D51Q27kadai.java
@@ -0,0 +1,7 @@
+enum TrafficLights{赤, 黃, 青}
+public class D51Q27kadai {
+    public static void main(String[] args) {
+        TrafficLights blue = TrafficLights.青;
+        System.out.println(blue);
+    }
+}
